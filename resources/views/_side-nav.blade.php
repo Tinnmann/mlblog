4619dddd7d01c0fb68e8982f4aa61f1337b5ca54
Tinnmann <tinmadzin@gmail.com>
2020-12-08 08:39:27 +0200

    icon size

--- resources/views/_side-nav.blade.php
+++ resources/views/_side-nav.blade.php
@@ -1,5 +1,5 @@
 <ul>
 	<li><a class="font-bold text-lgmb-4 block" href="/dashboard">
-		<ion-icon name="home-outline"></ion-icon>
+		<ion-icon name="home-outline" style="font-size:64px"></ion-icon>
 	</a></li>
 </ul>
